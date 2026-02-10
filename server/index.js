51026b0ef9d016ca9ceb7030dc92fa4725e7128a
rhectoroc <rhectoroc@gmail.com>
2026-02-10 17:40:16 -0400

    fix deploy

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Initialize Database Table
